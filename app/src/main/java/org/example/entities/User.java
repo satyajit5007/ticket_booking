223f069a66889c1177b2546042baa892570f0e8e
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-02-12 22:49:55 +0530

    entity

---
+++ app/src/main/java/org/example/entities/User.java
@@ -0,0 +1,12 @@
+package org.example.entities;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String password;
+    private String hashPassword;
+    private List<Ticket> ticketBooked;
+    private String userId;
+
+}
